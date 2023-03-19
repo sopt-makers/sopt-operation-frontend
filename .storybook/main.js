8662bd3f21afa5f4bcddac6e30c916e524ff45d8
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-03-19 18:03:49 +0900

    chore : Storybook initial setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
+    '@storybook/addon-mdx-gfm',
   ],
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
