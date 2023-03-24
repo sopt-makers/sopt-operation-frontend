f2f0e5f947f2788e77b8b6f433b19b5059711620
sohee <soheek@fairsquarelab.com>
2023-03-24 20:54:51 +0900

    chore: jest 초기 세팅

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig: Config = {
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  // moduleDirectories: ['node_modules', '<rootDir>/'],
+
+  // Handle module aliases
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(customJestConfig);
