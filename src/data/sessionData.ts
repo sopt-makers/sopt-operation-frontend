598586ce4817b8e57a0d1a33521e26be13d5076a
sohee <soheek@fairsquarelab.com>
2023-04-07 18:25:45 +0900

    feat: 세션선택 뷰 헤더 구현 및 데이터 처리

--- src/data/sessionData.ts
+++ src/data/sessionData.ts
@@ -1,8 +1,15 @@
 export const attendanceInit: Attendance = {
   round: 0,
   status: 'ABSENT',
-  date: '- -',
+  date: '-',
 };
+
+export const attendanceOptions: Array<{ label: string; value: ATTEND_STATUS }> =
+  [
+    { label: '출석', value: 'ATTENDANCE' },
+    { label: '지각', value: 'TARDY' },
+    { label: '결석', value: 'ABSENT' },
+  ];
 
 export const sessionDetailDummy: SessionDetail = {
   name: '테스트',
@@ -11,25 +18,25 @@ export const sessionDetailDummy: SessionDetail = {
   attribute: 'SEMINAR',
   members: [
     {
-      name: '소현',
-      university: '앙중대',
+      name: '소현여섯글자',
+      university: '중앙대중앙대',
       attendances: [
         {
           round: 1,
-          status: 'ABSENT',
+          status: 'ATTENDANCE',
           date: '23/01/01 11:00',
         },
         {
           round: 2,
-          status: 'ABSENT',
+          status: 'ATTENDANCE',
           date: '23/01/01 11:00',
         },
       ],
       score: 0,
     },
     {
-      name: '용택',
-      university: '숭실숭실',
+      name: '용택다섯자',
+      university: '한국외국어대학교',
       attendances: [
         {
           round: 1,
--- src/pages/attendanceAdmin/session/[id].tsx
+++ src/pages/attendanceAdmin/session/[id].tsx
@@ -5,30 +5,51 @@ import Button from '@/components/common/Button';
 import Footer from '@/components/common/Footer';
 import ListWrapper from '@/components/common/ListWrapper';
 import PartFilter from '@/components/common/PartFilter';
-import { attendanceInit, sessionDetailDummy } from '@/data/sessionData';
+import Select from '@/components/session/Select';
+import {
+  attendanceInit,
+  attendanceOptions,
+  sessionDetailDummy,
+} from '@/data/sessionData';
 import { precision } from '@/utils';
 
-function SessionDetailPage() {
-  const HEADER_LABELS = [
-    '순번',
-    '회원명',
-    '학교명',
-    '1차 출석 상태',
-    '1차 출석 일시',
-    '2차 출석 상태',
-    '2차 출석 일시',
-    '변동점수',
-  ];
+const HEADER_LABELS = [
+  'ㅤ순번ㅤ',
+  '회원명',
+  '학교명',
+  '1차 출석 상태',
+  '1차 출석 일시',
+  '2차 출석 상태',
+  '2차 출석 일시',
+  '변동점수',
+];
 
+function SessionDetailPage() {
   const [selectedPart, setSelectedPart] = useState<PART>('ALL');
+  const [session, setSession] = useState<SessionDetail>(sessionDetailDummy);
+  const [members, setMembers] = useState<Member[]>(sessionDetailDummy.members);
 
   const onChangePart = (part: PART) => {
     setSelectedPart(part);
   };
 
   return (
-    <>
-      <PartFilter selected={selectedPart} onChangePart={onChangePart} />
+    <StPageWrapper>
+      <StPageHeader>
+        <div className="session-info">
+          <h2>
+            <strong>1차 세미나</strong> 출석 관리
+          </h2>
+          <div className="attendance-info">
+            <p>출석 100</p>
+            <p>지각 29</p>
+            <p>결석 3</p>
+            <p>미정 45</p>
+          </div>
+        </div>
+        <PartFilter selected={selectedPart} onChangePart={onChangePart} />
+      </StPageHeader>
+
       <ListWrapper>
         <thead>
           <tr>
@@ -38,7 +59,7 @@ function SessionDetailPage() {
           </tr>
         </thead>
         <tbody>
-          {sessionDetailDummy.members.map((member, index) => {
+          {members.map((member, index) => {
             const firstRound =
               member.attendances.find((item) => item.round === 1) ??
               attendanceInit;
@@ -49,11 +70,27 @@ function SessionDetailPage() {
             return (
               <tr key={`${member.name}-${member.university}`}>
                 <td>{precision(index + 1, 2)}</td>
-                <td>{member.name}</td>
-                <td>{member.university}</td>
-                <td>{firstRound.status}</td>
+                <td className="member-name">
+                  <p>{member.name}</p>
+                </td>
+                <td className="member-university">
+                  <p>{member.university}</p>
+                </td>
+                <td>
+                  <Select
+                    selected={firstRound.status}
+                    options={attendanceOptions}
+                    onChange={(value) => console.log(value)}
+                  />
+                </td>
                 <td>{firstRound.date}</td>
-                <td>{secondRound.status}</td>
+                <td>
+                  <Select
+                    selected={secondRound.status}
+                    options={attendanceOptions}
+                    onChange={(value) => console.log(value)}
+                  />
+                </td>
                 <td>{secondRound.date}</td>
                 <td>{member.score}</td>
               </tr>
@@ -61,6 +98,7 @@ function SessionDetailPage() {
           })}
         </tbody>
       </ListWrapper>
+
       <Footer>
         <StFooterContents>
           <div className="code-wrap">
@@ -73,10 +111,46 @@ function SessionDetailPage() {
           </div>
         </StFooterContents>
       </Footer>
-    </>
+    </StPageWrapper>
   );
 }
 
+const StPageWrapper = styled.div`
+  .member-name p,
+  .member-university p {
+    max-width: 7.3rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin: 0 auto;
+  }
+  .member-university p {
+    max-width: 7.6rem;
+  }
+`;
+const StPageHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 6rem;
+  h2 {
+    font-size: 2rem;
+    font-weight: 600;
+    line-height: 2.5rem;
+    margin-bottom: 1.2rem;
+    color: ${({ theme }) => theme.color.grayscale.black60};
+    strong {
+      font-weight: 700;
+    }
+  }
+  .attendance-info {
+    display: flex;
+    gap: 1rem;
+    font-size: 1.4rem;
+    line-height: 1.7rem;
+    color: ${({ theme }) => theme.color.grayscale.gray100};
+  }
+`;
 const StFooterContents = styled.div`
   display: flex;
   align-items: center;
