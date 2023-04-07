488490e241d9f400c5a99672bd6b71ad0d8923ab
sohee <soheek@fairsquarelab.com>
2023-04-07 14:43:44 +0900

    feat: Button 구현

--- src/pages/attendanceAdmin/session/[id].tsx
+++ src/pages/attendanceAdmin/session/[id].tsx
@@ -1,3 +1,6 @@
+import styled from '@emotion/styled';
+
+import Button from '@/components/common/Button';
 import Footer from '@/components/common/Footer';
 import ListWrapper from '@/components/common/ListWrapper';
 import { attendanceInit, sessionDetailDummy } from '@/data/sessionData';
@@ -49,9 +52,47 @@ function SessionDetailPage() {
           })}
         </tbody>
       </ListWrapper>
-      <Footer>Footer</Footer>
+      <Footer>
+        <StFooterContents>
+          <div className="code-wrap">
+            <p>1차 출석코드</p>
+            <p>80182</p>
+          </div>
+          <div className="button-wrap">
+            <Button type="submit" text="1차 출석 시작하기" />
+            <Button type="submit" text="2차 출석 시작하기" disabled />
+          </div>
+        </StFooterContents>
+      </Footer>
     </>
   );
 }
+
+const StFooterContents = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .code-wrap {
+    display: flex;
+    align-items: center;
+    gap: 1.6rem;
+    font-size: 1.6rem;
+    line-height: 2.2rem;
+    font-weight: 600;
+    & > p:first-of-type {
+      color: ${({ theme }) => theme.color.grayscale.gray100};
+    }
+    & > p:last-of-type {
+      color: ${({ theme }) => theme.color.grayscale.white100};
+      background-color: ${({ theme }) => theme.color.main.purple100};
+      padding: 0.4rem 1rem;
+      border-radius: 0.4rem;
+    }
+  }
+  .button-wrap {
+    display: flex;
+    gap: 2.4rem;
+  }
+`;
 
 export default SessionDetailPage;
