5db93323e8fb00dcb0dbeac4bff0afc3826bba36
sohee <soheek@fairsquarelab.com>
2023-04-20 20:22:40 +0900

    fix: precision error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 export const precision = (num: number, precisionNum: number) => {
   const numToString = num + '';
   const numLength = numToString.length;
+
+  if (numLength > 2) return num;
   return '0'.repeat(precisionNum - numLength) + numToString;
 };
 
