a98dff2a794a8c30ac783637fc5163bcec6426e6
dudgns3tp <c_yh0327@naver.com>
2023-04-23 23:29:46 +0900

    chore: add sider

--- src/utils/nav.ts
+++ src/utils/nav.ts
@@ -3,5 +3,9 @@ export const MENU_LIST = [
     title: '출석 관리',
     subMenu: ['출석 세션', '출석 총점'],
     path: ['/attendanceAdmin/session', '/attendanceAdmin/totalScore'],
-  },
+  }, {
+    title:'공홈 관리',
+    subMenu: ['ABOUT'],
+    path: ['/orgAdmin/aboutManagement'],
+  }
 ];
