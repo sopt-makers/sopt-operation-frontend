ed9f59b4d0011a6d26085cabccd2a172fe89e0a9
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-04-24 19:12:55 +0900

    feat : 에러케이스 추가

--- src/services/api/lecture/index.ts
+++ src/services/api/lecture/index.ts
@@ -19,9 +19,9 @@ export const useGetSessionList = (
   part: string,
   authHeader: AuthHeader,
 ) => {
-  return useQuery<Lecture, Error>(
+  return useQuery(
     ['sessionList', generation, part, authHeader],
-    async () => {
+    async (): Promise<Lecture | ProjectError> => {
       try {
         const { data }: AxiosResponse<{ data: Lecture }> = await client.get(
           `/lectures?generation=${generation}&part=${part}`,
@@ -30,8 +30,22 @@ export const useGetSessionList = (
           },
         );
         return data.data;
-      } catch (error) {
-        throw error;
+      } catch (e) {
+        switch (e.response?.status) {
+          case 400:
+            throw {
+              status: 400,
+              error: '세션 정보를 불러오는데 실패했어요',
+            };
+          case 401:
+          case 402:
+          case 403:
+            throw { status: 403, error: '세션 정보 조회 권한이 없어요' };
+          case 404:
+          case 500:
+          default:
+            throw { status: 500, error: '알 수 없는 에러예요' };
+        }
       }
     },
   );
