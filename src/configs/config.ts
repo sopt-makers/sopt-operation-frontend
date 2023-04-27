fbd3cc0c6c165bd155c1c4b5886fc3524180ebf0
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-04-28 02:35:52 +0900

    feat : 환경변수 별 url 설정

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,4 +1,7 @@
-const API_URL = 'https://operation.api.dev.sopt.org/api/v1';
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL === 'PRODUCTION'
+    ? 'https://operation.api.sopt.org/api/v1'
+    : 'https://operation.api.dev.sopt.org/api/v1';
 const CLIENT_URL = 'https://operation.sopt.org';
 
 const config = {
