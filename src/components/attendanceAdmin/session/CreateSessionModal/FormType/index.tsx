eaa1302d370d1ae2f1b187e4c8e1327da0b5979b
Brokyeom <goindol40@gmail.com>
2023-06-13 02:43:51 +0900

    refactor : switch 조건부 렌더링 적용

--- src/components/attendanceAdmin/session/CreateSessionModal/FormType/index.tsx
+++ src/components/attendanceAdmin/session/CreateSessionModal/FormType/index.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
 
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+import IcDropdown from '@/components/common/icons/IcDropDown';
 import Input from '@/components/common/Input';
 
-const FormType = () => {
+interface Props {
+  formType: string;
+}
+
+const FormType = (props: Props) => {
+  const { formType } = props;
+
   const [sessionName, setSessionName] = useState<string>();
   const [sessionLocation, setSessionLocation] = useState<string>();
+  const [date, setDate] = useState<string>();
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -19,13 +31,54 @@ const FormType = () => {
     }
   };
 
-  return (
-    <Input>
-      <input
-        placeholder="세션 이름을 입력해주세요"
-        onChange={(e) => handleInputChange(e, '세션 이름')}></input>
-    </Input>
-  );
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+
+    if (date) {
+      const year = date.getFullYear();
+      const month = ('0' + (date.getMonth() + 1)).slice(-2);
+      const day = ('0' + date.getDate()).slice(-2);
+
+      const formattedDate = `${year}/${month}/${day}`;
+      setDate(formattedDate);
+    }
+  };
+
+  const handleForm = () => {
+    switch (formType) {
+      case 'name':
+        return (
+          <>
+            <input
+              placeholder="세션 이름을 입력해주세요"
+              onChange={(e) => handleInputChange(e, '세션 이름')}
+            />
+          </>
+        );
+      case 'location':
+        return (
+          <>
+            <input
+              placeholder="세션이 열리는 장소를 입력해주세요"
+              onChange={(e) => handleInputChange(e, '세션 장소')}></input>
+          </>
+        );
+      case 'date':
+        return (
+          <>
+            <DatePicker
+              placeholderText="세션 날짜를 선택해주세요"
+              dateFormat="yyyy/MM/dd"
+              selected={selectedDate}
+              onChange={handleDateChange}
+            />
+            <IcDropdown color={date ? '#3C3D40' : '#C0C5C9'} />
+          </>
+        );
+    }
+  };
+
+  return <Input>{handleForm()}</Input>;
 };
 
 export default FormType;
