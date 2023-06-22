9dc1d20d651cba6e9c501979800de83f65f78e42
Brokyeom <goindol40@gmail.com>
2023-06-23 01:02:55 +0900

    chore : 불필요한 기능 코드 삭제

--- src/services/api/lecture/index.ts
+++ src/services/api/lecture/index.ts
@@ -61,11 +61,6 @@ export const useGetSessionList = (
   );
 };
 
-export const deleteSession = async (lectureId: string | undefined) => {
-  const res = await client.delete(`/lectures/${lectureId}`);
-  return res;
-};
-
 export const getSessionDetail = async (
   lectureId: number,
   authHeader: AuthHeader,
