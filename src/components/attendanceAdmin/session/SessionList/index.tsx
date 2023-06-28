0213620b4a30deb739e5a1a56477fec1eab51fb3
Brokyeom <goindol40@gmail.com>
2023-06-28 13:13:53 +0900

    Revert "chore : 불필요한 요소, 의존성 배열 삭제"
    This reverts commit 7a5a3fff7b7d6d36757e07e9e21097bcd1bf760e.

--- src/components/attendanceAdmin/session/SessionList/index.tsx
+++ src/components/attendanceAdmin/session/SessionList/index.tsx
@@ -48,7 +48,7 @@ function SessionList() {
   const [selectedPart, setSelectedPart] = useState<PART>('ALL');
   const [lectureData, setLectureData] = useState<LectureList[]>([]);
 
-  const { data, isLoading, error } = useGetSessionList(
+  const { data, isLoading, isError, error } = useGetSessionList(
     32,
     selectedPart,
     getAuthHeader(),
@@ -58,7 +58,7 @@ function SessionList() {
     if (data && 'lectures' in data) {
       setLectureData(data.lectures);
     }
-  }, [data]);
+  }, [data, error, isError, router]);
 
   const handleManageClick = (lectureId: number) => {
     router.push(`/attendanceAdmin/session/${lectureId}`);
