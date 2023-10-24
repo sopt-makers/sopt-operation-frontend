3b5d0d840dc9960efc279d79312c3746c32f031b
Brokyeom <goindol40@gmail.com>
2023-10-24 21:52:13 +0900

    style : Nav mds 토대의 스타일 변경

--- src/utils/nav.ts
+++ src/utils/nav.ts
@@ -1,12 +1,19 @@
+import { IcAlarmMenu, IcAttendanceMenu, IcOrgMenu } from '@/assets/icons';
+
 export const MENU_LIST = [
   {
     title: '출석 관리',
+    MenuIcon: IcAttendanceMenu,
     subMenu: ['출석 세션', '출석 총점'],
     path: ['/attendanceAdmin/session', '/attendanceAdmin/totalScore'],
   },
   {
     title: '공홈 관리',
-    subMenu: ['ABOUT'],
+    MenuIcon: IcOrgMenu,
     path: ['/orgAdmin/aboutTabManagement'],
+  },
+  {
+    title: '알림 관리',
+    MenuIcon: IcAlarmMenu,
   },
 ];
