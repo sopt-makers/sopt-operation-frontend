5fed58bcd896dc77c2a40495cc70f15989f291fd
Brokyeom <goindol40@gmail.com>
2023-10-24 23:29:46 +0900

    chore : 기수 선택 드롭다운 요소에 대응하는 util 추가

--- src/utils/generation.ts
+++ src/utils/generation.ts
@@ -1,3 +1,13 @@
+import {
+  Generation32Logo,
+  Generation33Logo,
+} from '@/assets/icons/IcGenerationLogo';
+
 export const ACTIVITY_GENRATION: string = '33';
 
 export const GENERATION_LIST: string[] = ['33', '32'];
+
+export const GENERATION_DROPDOWN = [
+  { generation: '33', logo: Generation33Logo, name: 'DO SOPT' },
+  { generation: '32', logo: Generation32Logo, name: 'GO SOPT' },
+];
