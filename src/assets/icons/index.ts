06e6dd120d1117e103dfc18b8401cc9ba89c580e
Brokyeom <goindol40@gmail.com>
2023-10-31 22:09:07 +0900

    fix : autofix error

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,7 +4,6 @@ export { default as IcCheckBox } from './IcCheckBox';
 export { default as IcDeleteFile } from './IcDeleteFile.svg';
 export { default as IcGoPrev } from './IcGoPrev.svg';
 export { default as IcModalClose } from './IcModalClose.svg';
-export { default as IcNavMenu } from './IcNavMenu.svg';
 export { default as IcNewDropdown } from './IcNewDropdown.svg';
-export { default as IcUpload } from './IcUpload.svg';
 export { default as IcOrgMenu } from './IcOrgMenu.svg';
+export { default as IcUpload } from './IcUpload.svg';
