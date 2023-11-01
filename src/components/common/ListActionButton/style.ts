8b4d4fafdf5289652071c85fff886bb2c6575ebc
Brokyeom <goindol40@gmail.com>
2023-11-01 19:50:22 +0900

    style : ListActionButton mds 기반으로 변경

--- src/components/common/ListActionButton/style.ts
+++ src/components/common/ListActionButton/style.ts
@@ -1,27 +1,31 @@
 import styled from '@emotion/styled';
-
-import { body2 } from '@/styles/fonts';
+import { colors } from '@sopt-makers/colors';
 
 export const StButton = styled.button`
-  ${body2}
+  text-align: center;
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 140%; /* 1.96rem */
+  letter-spacing: -0.028rem;
   transition: transform 0.1s;
+
   display: inline-block;
 
   padding: 0.6rem 1rem;
-  border: 0.1rem solid ${({ theme }) => theme.color.grayscale.gray60};
-  border-radius: 1.6rem;
-  background-color: ${({ theme }) => theme.color.grayscale.gray20};
-  color: ${({ theme }) => theme.color.grayscale.black40};
+  border: 0.1rem solid ${colors.gray300};
+  border-radius: 0.8rem;
+  background-color: ${colors.gray600};
+  color: ${colors.gray10};
 
   &:disabled {
-    background-color: ${({ theme }) => theme.color.grayscale.gray10};
-    border: 1px solid ${({ theme }) => theme.color.grayscale.gray20};
-    color: ${({ theme }) => theme.color.grayscale.gray30};
+    background-color: ${colors.gray800};
+    border: none;
+    color: ${colors.gray500};
     cursor: default;
   }
   cursor: pointer;
   &:not(:disabled):hover {
     transform: scale(1.15);
-    border-color: ${({ theme }) => theme.color.grayscale.gray100};
   }
 `;
