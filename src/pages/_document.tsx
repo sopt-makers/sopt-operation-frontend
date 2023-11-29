370b1bdba07ea61a882a5ba7aab2c7a479c61326
sohee <soheek@fairsquarelab.com>
2023-11-29 18:27:38 +0900

    chore: meta data 구조 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="SOPT Operation Service" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
