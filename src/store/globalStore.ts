851c60ea5308b96704af1037f8e3a70e04dc554f
Brokyeom <goindol40@gmail.com>
2023-11-30 16:37:41 +0900

    chore : 불필요한 recoil 요소 삭제

--- src/store/globalStore.ts
+++ src/store/globalStore.ts
@@ -1,13 +1,4 @@
 import { atom, DefaultValue, selector } from 'recoil';
-import { recoilPersist } from 'recoil-persist';
-
-const sessionStorage =
-  typeof window !== 'undefined' ? window.sessionStorage : undefined;
-
-const { persistAtom } = recoilPersist({
-  key: 'adminStatusStorage',
-  storage: sessionStorage,
-});
 
 export const user = atom<User>({
   key: 'userData',
@@ -15,22 +6,5 @@ export const user = atom<User>({
     id: 0,
     adminStatus: 'NOT_CERTIFIED',
     name: '',
-  },
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const adminStatusState = selector({
-  key: 'adminStatusState',
-  get: ({ get }) => {
-    const userData = get(user);
-    return userData.adminStatus;
-  },
-  set: ({ set }, newValue) => {
-    if (newValue instanceof DefaultValue) return;
-
-    set(user, (prevUserData) => ({
-      ...prevUserData,
-      adminStatus: newValue,
-    }));
   },
 });
