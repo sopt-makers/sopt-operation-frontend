aaf53264ea0f091cb4fd64579a7991d8d5652188
Brokyeom <goindol40@gmail.com>
2024-07-19 22:47:09 +0900

    feat : 알림 발송 방법에 따른 버튼 커스텀 추가

--- src/pages/alarmAdmin/index.tsx
+++ src/pages/alarmAdmin/index.tsx
@@ -1,3 +1,6 @@
+import { css } from '@emotion/react';
+import { IconArrowUpRight, IconClock, IconPlus } from '@sopt-makers/icons';
+import { Button } from '@sopt-makers/ui';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
@@ -8,8 +11,11 @@ import Loading from '@/components/common/Loading';
 import Modal from '@/components/common/modal';
 import { currentGenerationState } from '@/recoil/atom';
 import { useGetAlarmList } from '@/services/api/alarm/query';
+import zIndex from '@/utils/zIndex';
 
 function AlarmAdminPage() {
+  const [isSendTypeSelectorOpen, setIsSendTypeSelectorOpen] =
+    useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const currentGeneration = useRecoilValue(currentGenerationState);
@@ -27,14 +33,49 @@ function AlarmAdminPage() {
     refetch();
   };
 
-  if (isLoading || !data) return <Loading />;
+  // if (isLoading || !data) return <Loading />;
   return (
     <>
       <AlarmList data={data} refetch={refetchAlarmList} />
-      <FloatingButton
-        content={<>알림 생성하기</>}
-        onClick={() => setIsModalOpen(!isModalOpen)}
-      />
+      <section css={floatingButtonsCss}>
+        {isSendTypeSelectorOpen && (
+          <article
+            css={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '4px',
+
+              '& > button': { width: '100%' },
+            }}>
+            <Button
+              size="md"
+              LeftIcon={IconArrowUpRight}
+              onClick={() => {
+                setIsModalOpen(!isModalOpen);
+                setIsSendTypeSelectorOpen(false);
+              }}>
+              즉시 발송
+            </Button>
+            <Button
+              size="md"
+              LeftIcon={IconClock}
+              onClick={() => {
+                setIsModalOpen(!isModalOpen);
+                setIsSendTypeSelectorOpen(false);
+              }}>
+              예약 발송
+            </Button>
+          </article>
+        )}
+        <Button
+          size="md"
+          LeftIcon={IconPlus}
+          onClick={() => setIsSendTypeSelectorOpen((prev) => !prev)}>
+          알림 생성하기
+        </Button>
+      </section>
+
       {isModalOpen && (
         <Modal>
           <CreateAlarmModal onClose={handleModalClose} />
@@ -45,3 +86,15 @@ function AlarmAdminPage() {
 }
 
 export default AlarmAdminPage;
+
+const floatingButtonsCss = css({
+  position: 'fixed',
+  right: '60px',
+  bottom: '60px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
+
+  zIndex: zIndex.footer,
+});
