279db1d165df992062761c5d98a74b3d882780ed
HyeongKyeom Kim <97586683+Brokyeom@users.noreply.github.com>
2024-08-19 21:20:55 +0900

    활동기수 35기 기준으로 수정 (#108)

--- src/assets/icons/SoptLogos/index.ts
+++ src/assets/icons/SoptLogos/index.ts
@@ -1,3 +1,4 @@
+export { default as AndSoptLogo } from './AndSoptLogo.svg';
 export { default as DoSoptLogo } from './DoSoptLogo.svg';
 export { default as GoSoptLogo } from './GoSoptLogo.svg';
 export { default as NowSoptLogo } from './NowSoptLogo.svg';
