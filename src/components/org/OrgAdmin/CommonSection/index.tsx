a4d85a1e222480e60d2d8a5b43a70a75bb9f111e
Jeon Eonseok <jseokeon@gmail.com>
2024-10-15 23:41:24 +0900

    feat: 실시간 브랜딩 컬러 변경되도룩 구현

--- src/components/org/OrgAdmin/CommonSection/index.tsx
+++ src/components/org/OrgAdmin/CommonSection/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
   StColorPreview,
   StColorWrapper,
@@ -17,6 +19,11 @@ import {
 } from './style';
 
 const CommonSection = () => {
+  const [mainColor, setMainColor] = useState('');
+  const [lowColor, setLowColor] = useState('');
+  const [highColor, setHighColor] = useState('');
+  const [subGrayColor, setSubGrayColor] = useState('');
+
   return (
     <StContainer>
       <StWrapper>
@@ -104,30 +111,50 @@ const CommonSection = () => {
           <StInputBox>
             <StInputLabel>키컬러 (메인)</StInputLabel>
             <StColorWrapper>
-              <StInput type="text" placeholder="ex. #ffffff" />
-              <StColorPreview />
+              <StInput
+                type="text"
+                placeholder="ex. #ffffff"
+                value={mainColor}
+                onChange={(e) => setMainColor(e.target.value)}
+              />
+              <StColorPreview style={{ backgroundColor: mainColor }} />
             </StColorWrapper>
           </StInputBox>
           <StInputBox>
             <StInputLabel>키컬러 (저명도)</StInputLabel>
             <StColorWrapper>
-              <StInput type="text" placeholder="ex. #ffffff" />
-              <StColorPreview />
+              <StInput
+                type="text"
+                placeholder="ex. #ffffff"
+                value={lowColor}
+                onChange={(e) => setLowColor(e.target.value)}
+              />
+              <StColorPreview style={{ backgroundColor: lowColor }} />
             </StColorWrapper>
           </StInputBox>
           <StInputBox>
             <StInputLabel>키컬러 (고명도)</StInputLabel>
             <StColorWrapper>
-              <StInput type="text" placeholder="ex. #ffffff" />
-              <StColorPreview />
+              <StInput
+                type="text"
+                placeholder="ex. #ffffff"
+                value={highColor}
+                onChange={(e) => setHighColor(e.target.value)}
+              />
+              <StColorPreview style={{ backgroundColor: highColor }} />
             </StColorWrapper>
           </StInputBox>
           <StInputBox>
             <StInputLabel>서브 컬러 (강조 그레이 컬러)</StInputLabel>
             <StDescription></StDescription>
             <StColorWrapper>
-              <StInput type="text" placeholder="ex. #ffffff" />
-              <StColorPreview />
+              <StInput
+                type="text"
+                placeholder="ex. #ffffff"
+                value={subGrayColor}
+                onChange={(e) => setSubGrayColor(e.target.value)}
+              />
+              <StColorPreview style={{ backgroundColor: subGrayColor }} />
             </StColorWrapper>
           </StInputBox>
         </StInputWrapper>
