dc4137b9355ad208feb74b0fff6fcdb8709b1e78
lydiacho <sheecho0804@ewhain.net>
2024-10-23 04:05:22 +0900

    design: 핵심가치 섹션 퍼블리싱

--- src/components/org/OrgAdmin/AboutSection/style.ts
+++ src/components/org/OrgAdmin/AboutSection/style.ts
@@ -4,6 +4,44 @@ import { fontsObject } from '@sopt-makers/fonts';
 import { TextField } from '@sopt-makers/ui';
 
 export const StContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
   padding: 50px 0;
-  background-color: white;
+`;
+
+// TODO : StTitleWrapper, StTitle common 분리
+
+export const StWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const StTitle = styled.h2`
+  ${fontsObject.TITLE_3_24_SB}
+  margin-bottom: 10px;
+  color: ${colors.white};
+`;
+
+export const StInputLabel = styled.label`
+  ${fontsObject.LABEL_3_14_SB};
+  margin-bottom: 8px;
+  color: ${colors.white};
+`;
+
+export const StDescription = styled.p`
+  ${fontsObject.LABEL_4_12_SB};
+  margin-bottom: 13px;
+  color: ${colors.gray300};
+`;
+
+interface StInputProps {
+  hasValue?: boolean;
+}
+
+export const StInput = styled(TextField)<StInputProps>`
+  width: 338px;
+  color: ${({ hasValue = true }) =>
+    hasValue ? `${colors.white}` : `${colors.gray300}`};
 `;
