50649f4360e882e835071529b211f2c135eb0ea9
wuzoo <wndyd9911@gmail.com>
2024-10-23 23:58:08 +0900

    feat: Delete, AddNews modal publishing

---
+++ src/components/org/OrgAdmin/home/Modal.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { TextField } from '@sopt-makers/ui';
+
+import Modal from '@/components/common/modal';
+import ImageInput from '@/components/org/OrgAdmin/home/ImageInput';
+import {
+  StAddModalBtnWrapper,
+  StAddModalContainer,
+  StCancelButton,
+  StDeleteButton,
+  StModalBtnWrapper,
+  StModalContainer,
+} from '@/components/org/OrgAdmin/home/Modal.style';
+
+type DeleteModalProps = {
+  isOpen: boolean;
+  onCancel?: () => void;
+  onDelete?: () => void;
+};
+
+export const DeleteModal = ({
+  isOpen,
+  onCancel,
+  onDelete,
+}: DeleteModalProps) => {
+  return (
+    isOpen && (
+      <Modal>
+        <StModalContainer>
+          <h2>삭제하시겠습니까?</h2>
+          <p>삭제한 최신 소식은 복구되지 않아요.</p>
+          <StModalBtnWrapper>
+            <StCancelButton onClick={onCancel}>취소</StCancelButton>
+            <StDeleteButton onClick={onDelete}>삭제</StDeleteButton>
+          </StModalBtnWrapper>
+        </StModalContainer>
+      </Modal>
+    )
+  );
+};
+
+export const AddNewsModal = () => {
+  return (
+    <Modal>
+      <StAddModalContainer>
+        <ImageInput
+          label="이미지"
+          description="이미지는 00x00 비율로 올려주세요."
+        />
+        <TextField
+          value=""
+          required
+          labelText="최신 소식 제목"
+          placeholder="최신 소식의 제목을 입력하세요."
+        />
+        <TextField
+          value=""
+          labelText="링크 첨부"
+          placeholder="링크를 입력하세요."
+        />
+
+        <StAddModalBtnWrapper>
+          <StCancelButton>취소</StCancelButton>
+          <StDeleteButton>추가</StDeleteButton>
+        </StAddModalBtnWrapper>
+      </StAddModalContainer>
+    </Modal>
+  );
+};
