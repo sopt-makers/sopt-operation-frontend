59a0024301bdc7f1bbc7add31e14c83f141209cf
HyeongKyeom Kim <97586683+Brokyeom@users.noreply.github.com>
2024-10-26 22:28:04 +0900

    [FEATURE] 알림 발송 API 연결 (#116)
    * yarn install version 4.5.0
    
    * Revert "feat: 알림 발송 API 연결"
    
    This reverts commit d25d07e0d38b4cc79c14c4a9011783fc945f8f66.
    
    * feat: 알림 발송 API 연결
    
    * fix: remoce legacy postAlarm function.
    
    * fix: remove legacy Alarm Modal component.
    
    * feat: 알림 앱링크 연결
    
    * refactor: selectOptions 항목 utils로 이동
    
    * lint
    
    * feat: CSV 파일 업로드 로직 이식
    
    * fix: 불필요한 코드 삭제
    
    * feat: CSV 파일 검사 로직 추가
    
    * Add onClose
    
    * fix: csv 조건문 수정

--- src/components/alarmAdmin/CreateAlarmModal/style.ts
+++ src/components/alarmAdmin/CreateAlarmModal/style.ts
@@ -1,11 +1,14 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { colors } from '@sopt-makers/colors';
+import { fontsObject } from '@sopt-makers/fonts';
+import { IconArrowUpRight } from '@sopt-makers/icons';
 
 export const StAlarmModalWrapper = styled.section`
-  width: 50.4rem;
+  width: 64rem;
 
   & > main {
-    padding: 1.6rem 3rem 3.2rem 3rem;
+    padding: 2.6rem 3rem;
 
     & > .type_selector {
       display: flex;
@@ -66,12 +69,9 @@ export const StCsvUploader = styled.div`
   width: 100%;
 
   padding: 1rem 1.4rem;
+  margin-top: 0.8rem;
 
-  font-size: 1.6rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 2.4rem; /* 150% */
-  letter-spacing: -0.032rem;
+  ${fontsObject.BODY_2_16_M};
 
   color: ${colors.gray400};
   background-color: ${colors.gray700};
@@ -140,4 +140,191 @@ export const StTextArea = styled.textarea`
   &:focus {
     cursor: default;
   }
+`;
+
+export const SelectWrapper = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+
+  margin-top: 1.6rem;
+`;
+
+export const SendTimeWrapper = styled.div`
+  display: flex;
+  gap: 2rem;
+
+  margin-top: 1.6rem;
+
+  & .react-datepicker-wrapper {
+    height: 0;
+  }
+
+  & .react-datepicker__navigation {
+    width: 3.2rem;
+  }
+`;
+
+export const AttachWrapper = styled.div`
+  width: 100%;
+  margin-top: 1.6rem;
+`;
+
+export const AvailableCheckLink = styled.a`
+  display: flex;
+  width: max-content;
+  align-items: center;
+
+  margin-top: 0.2rem;
+
+  border-bottom: 1px solid ${colors.gray100};
+  cursor: pointer;
+  color: ${colors.gray100};
+`;
+
+export const StyledIconArrowUpRight = styled(IconArrowUpRight)`
+  width: 1.6rem;
+`;
+
+export const AvailableCheckText = styled.span`
+  ${fontsObject.LABEL_5_11_SB}
+`;
+
+export const AttachOptionButtonList = styled.ul`
+  display: flex;
+  gap: 0.6rem;
+
+  margin-top: 0.8rem;
+`;
+
+export const OptionalInputWrapper = styled.div<{ attachOption: string }>`
+  width: 100%;
+  margin-top: ${({ attachOption }) => attachOption !== '첨부 안함' && '0.8rem'};
+`;
+
+export const triggerCSS = css`
+  background-color: ${colors.gray700};
+  width: 18rem;
+`;
+
+export const inputCSS = css`
+  & input {
+    background-color: ${colors.gray700};
+    width: 100%;
+  }
+`;
+
+export const textAreaCSS = css`
+  margin-top: 0.8rem;
+  & div {
+    background-color: ${colors.gray700};
+    width: 100%;
+    & textarea {
+      background-color: ${colors.gray700};
+    }
+  }
+`;
+
+export const sendSelectCSS = css`
+  width: 18rem;
+  margin-top: 0.8rem;
+  & button {
+    width: 100%;
+  }
+  & ul {
+    background-color: ${colors.gray600};
+    width: 100%;
+    & button {
+      width: 100%;
+    }
+    & button:hover {
+      background-color: ${colors.gray500};
+    }
+  }
+`;
+
+export const sendTriggerCSS = css`
+  background-color: ${colors.gray700};
+  width: 100%;
+`;
+
+export const reserveDateSelectCSS = css`
+  width: 28rem;
+  & button {
+    width: 100%;
+  }
+  & ul {
+    background-color: ${colors.gray600};
+    width: 100%;
+    & button {
+      width: 100%;
+    }
+    & button:hover {
+      background-color: ${colors.gray500};
+    }
+  }
+`;
+
+export const datePickerWrapperCSS = css`
+  height: 4.8rem;
+`;
+
+export const reserveTimeSelectCSS = (isBannedTime: boolean) => css`
+  width: 28rem;
+  & button {
+    width: 100%;
+    & > div:not(ul div) {
+      border: ${isBannedTime && `1px solid ${colors.error}`};
+    }
+  }
+  & ul {
+    background-color: ${colors.gray600};
+    width: 100%;
+    & button {
+      width: 100%;
+    }
+    & button:hover {
+      background-color: ${colors.gray500};
+    }
+  }
+`;
+
+export const reserveTimeTriggerCSS = (isBannedTime: boolean) => css`
+  background-color: ${colors.gray700};
+  width: 100%;
+  & p {
+    color: ${isBannedTime && colors.error};
+  }
+`;
+
+export const reserveDateTriggerCSS = (selectedDate: Date | null) => css`
+  background-color: ${colors.gray700};
+  width: 100%;
+  & p {
+    color: ${selectedDate ? colors.white : colors.gray300};
+  }
+`;
+
+export const deepLinkSelectCSS = css`
+  width: 100%;
+  & button {
+    width: 100%;
+  }
+  & ul {
+    background-color: ${colors.gray600};
+    width: 100%;
+    & button:hover {
+      background-color: ${colors.gray500};
+    }
+  }
+`;
+
+export const deepLinkTriggerCSS = css`
+  background-color: ${colors.gray700};
+  width: 100%;
 `;
