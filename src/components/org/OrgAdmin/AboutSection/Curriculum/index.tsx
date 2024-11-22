3ee4ed0faba802228bb7e94c0729a9d5b9e4bef5
lydiacho <sheecho0804@ewhain.net>
2024-11-22 17:10:30 +0900

    feat: curriculum hook form 연결

--- src/components/org/OrgAdmin/AboutSection/Curriculum/index.tsx
+++ src/components/org/OrgAdmin/AboutSection/Curriculum/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
 
 import { PART_LIST } from '@/utils/org';
 
@@ -7,26 +8,19 @@ import { StInput, StTitle, StWrapper } from '../style';
 import { StContentWrapper, StItem, StList, StWeek } from './style';
 
 const Curriculum = () => {
-  const initialCurriculum = PART_LIST.reduce(
+  const { register } = useFormContext();
+  const CURRICULUM = PART_LIST.reduce(
     (acc, part) => {
-      acc[part] = Array.from({ length: 8 }, () => '');
+      acc[part] = Array.from({ length: 8 });
       return acc;
     },
     {} as Record<string, string[]>,
   );
 
   const [selectedPart, setSelectedPart] = useState('기획');
-  const [curriculum, setCurriculum] = useState(initialCurriculum);
 
   const handleSetSelectedPart = (value: string) => {
     setSelectedPart(value);
-  };
-
-  const handleChangeInput = (idx: number, value: string) => {
-    setCurriculum((prev) => ({
-      ...prev,
-      [selectedPart]: prev[selectedPart].map((v, i) => (i === idx ? value : v)),
-    }));
   };
 
   return (
@@ -38,15 +32,14 @@ const Curriculum = () => {
           onSetSelectedPart={handleSetSelectedPart}
         />
         <StList>
-          {curriculum[selectedPart].map((curr, idx) => (
+          {CURRICULUM[selectedPart].map((_, idx) => (
             <StItem key={`${selectedPart} week${idx + 1}`}>
               <StWeek htmlFor={`${selectedPart} week${idx + 1}`}>
                 0{idx + 1}
               </StWeek>
               <StInput
+                {...register(`partCurriculum.${selectedPart}.${idx}`)}
                 id={`${selectedPart} week${idx}`}
-                value={curr}
-                onChange={(e) => handleChangeInput(idx, e.currentTarget.value)}
                 style={{ width: '553px' }}
                 placeholder={`${selectedPart} 파트 ${idx + 1}주차 커리큘럼을 작성해주세요.`}
               />
