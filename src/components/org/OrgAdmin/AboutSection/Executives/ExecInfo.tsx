51c57dd08b2a886e68335e146a4967bf39ab32ed
lydiacho <sheecho0804@ewhain.net>
2024-11-28 01:19:43 +0900

    fix: ExecInfo form label 수정 및 required 추가

--- src/components/org/OrgAdmin/AboutSection/Executives/ExecInfo.tsx
+++ src/components/org/OrgAdmin/AboutSection/Executives/ExecInfo.tsx
@@ -1,5 +1,7 @@
 import { useFormContext } from 'react-hook-form';
 
+import { VALIDATION_CHECK } from '@/utils/org';
+
 import MyDropzone from '../../MyDropzone';
 import IcBehanceLogo from '../assets/IcBehanceLogo';
 import IcGithubLogo from '../assets/IcGithubLogo';
@@ -15,7 +17,10 @@ interface ExecInfoProps {
 
 const ExecInfo = ({ selectedExec }: ExecInfoProps) => {
   const method = useFormContext();
-  const { register } = method;
+  const {
+    register,
+    formState: { errors },
+  } = method;
 
   return (
     <>
@@ -24,46 +29,58 @@ const ExecInfo = ({ selectedExec }: ExecInfoProps) => {
         <StDescription>사진은 1:1 비율로 올려주세요.</StDescription>
         <MyDropzone
           method={method}
-          label={`${selectedExec}.profileImageFileName`}
+          label={`${selectedExec}_profileImageFileName`}
           width="168px"
           height="168px"
           shape="circle"
         />
       </StPhotoWrapper>
       <StInput
-        {...register(`${selectedExec}.name`)}
+        {...register(`${selectedExec}_name`, {
+          required: true && VALIDATION_CHECK.required.errorText,
+        })}
+        isError={errors[`${selectedExec}_name`]?.message !== undefined}
+        errorMessage={errors[`${selectedExec}_name`]?.message as string}
         labelText="이름"
         placeholder="ex. 김솝트"
       />
       <StInput
-        {...register(`${selectedExec}.affiliation`)}
+        {...register(`${selectedExec}_affiliation`, {
+          required: true && VALIDATION_CHECK.required.errorText,
+        })}
+        isError={errors[`${selectedExec}_affiliation`]?.message !== undefined}
+        errorMessage={errors[`${selectedExec}_affiliation`]?.message as string}
         labelText="소속"
         placeholder="ex. 솝트대학교 / 솝트컴퍼니 / 앱잼 프로덕트명"
       />
       <StInput
-        {...register(`${selectedExec}.introduction`)}
+        {...register(`${selectedExec}_introduction`, {
+          required: true && VALIDATION_CHECK.required.errorText,
+        })}
+        isError={errors[`${selectedExec}_introduction`]?.message !== undefined}
+        errorMessage={errors[`${selectedExec}_introduction`]?.message as string}
         labelText="한 줄 소개"
         placeholder="ex. 새로운 도전을 위해 과감히 용기내는 사람"
       />
       <StSNSWrapper>
         <span>SNS</span>
         <SNSInput
-          label={`${selectedExec}.sns.email`}
+          label={`${selectedExec}_sns_email`}
           icon={IcMailLogo}
           placeholder="ex. 000@sopt.org"
         />
         <SNSInput
-          label={`${selectedExec}.sns.linkedin`}
+          label={`${selectedExec}_sns_linkedin`}
           icon={IcLinkedinLogo}
           placeholder="ex. https://www.linkedin.com/..."
         />
         <SNSInput
-          label={`${selectedExec}.sns.github`}
+          label={`${selectedExec}_sns_github`}
           icon={IcGithubLogo}
           placeholder="ex. https://github.com/..."
         />
         <SNSInput
-          label={`${selectedExec}.sns.behance`}
+          label={`${selectedExec}_sns_behance`}
           icon={IcBehanceLogo}
           placeholder="ex. https://www.behance.net/..."
         />
