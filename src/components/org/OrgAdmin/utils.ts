efb9dce3227e0dd4e0d4b2d0ad123449b1ebaf9e
Jeon Eonseok <jseokeon@gmail.com>
2024-12-26 22:59:31 +0900

    feat: 소개 탭 validate 체크 함수 생성

--- src/components/org/OrgAdmin/utils.ts
+++ src/components/org/OrgAdmin/utils.ts
@@ -1,6 +1,12 @@
 import type { FieldValues } from 'react-hook-form';
 
-import { VALIDATION_CHECK } from '@/utils/org';
+import {
+  type EXEC_TYPE,
+  type PART_KO,
+  PART_LIST,
+  VALIDATION_CHECK,
+  임원진_LIST,
+} from '@/utils/org';
 
 import type { Group } from './types';
 
@@ -46,6 +52,66 @@ export const validationCommonInputs = (
 
       if (name.includes('YB')) setGroup('YB');
       else if (name.includes('OB')) setGroup('OB');
+
+      break;
+    }
+  }
+
+  return isAllFilled;
+};
+
+export const validationAboutInputs = (
+  getValues: (payload?: string | string[]) => FieldValues,
+  setError: (name: string, error: { type: string; message: string }) => void,
+  setSelectedPartInHomeTap: (part: PART_KO) => void,
+  setSelectedExec: (member: EXEC_TYPE) => void,
+) => {
+  const {
+    headerImageFileName,
+    coreValue1,
+    coreValue2,
+    coreValue3,
+    partCurriculum,
+    member,
+  } = getValues();
+
+  const fieldsToValidate = [
+    { name: 'headerImageFileName', value: headerImageFileName },
+    ...[coreValue1, coreValue2, coreValue3].flatMap((coreValue, idx) =>
+      ['imageFileName', 'value', 'description'].map((key) => ({
+        name: `coreValue${idx + 1}.${key}`,
+        value: coreValue?.[key],
+      })),
+    ),
+    ...PART_LIST.flatMap((part) =>
+      Array.from({ length: 8 }).map((_, idx) => ({
+        name: `partCurriculum.${part}.${idx}`,
+        value: partCurriculum?.[part]?.[idx],
+      })),
+    ),
+    ...[...임원진_LIST, ...PART_LIST].flatMap((item) =>
+      ['profileImageFileName', 'name', 'introduction'].map((key) => ({
+        name: `member.${item}.${key}`,
+        value: member?.[item]?.[key],
+      })),
+    ),
+  ];
+
+  let isAllFilled = true;
+
+  for (const { name, value } of fieldsToValidate) {
+    if (!value) {
+      isAllFilled = false;
+
+      setError(name, {
+        type: 'required',
+        message: VALIDATION_CHECK.required.errorText,
+      });
+
+      if (name.includes('partCurriculum'))
+        setSelectedPartInHomeTap(name.split('.')[1] as PART_KO);
+      else if (name.includes('member'))
+        setSelectedExec(name.split('.')[1] as EXEC_TYPE);
 
       break;
     }
