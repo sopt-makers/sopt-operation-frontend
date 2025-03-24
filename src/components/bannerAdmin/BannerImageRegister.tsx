2d73533395cd86b724d0fd43df68f5a937d3f929
kimkyuhong <bobo3766@naver.com>
2025-03-24 21:10:42 +0900

    feat: 배너 이미지 업로드 컴포넌트 분리

---
+++ src/components/bannerAdmin/BannerImageRegister.tsx
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled';
+import { Button } from '@sopt-makers/ui';
+import { useFormContext } from 'react-hook-form';
+
+import {
+  StContentWrapper,
+  StDescription,
+  StDescriptionTitle,
+  StInputLabel,
+} from '@/components/bannerAdmin/CreateBannerModal';
+import ImageDropZone from '@/components/bannerAdmin/ImageDropZone';
+import RequiredIcon from '@/components/org/OrgAdmin/assets/RequiredIcon';
+
+const BannerImageRegister = () => {
+  const method = useFormContext();
+
+  return (
+    <StContentWrapper>
+      <StInputLabel>
+        <span>이미지 등록</span>
+        <RequiredIcon />
+      </StInputLabel>
+
+      <StDescriptionWrapper>
+        <StDescription>
+          <StDescriptionTitle>[PC]</StDescriptionTitle> 이미지는 1824*328 px
+          크기로 올려주세요.
+          <p>(형식: PNG, 용량: 5MB 이내)</p>
+        </StDescription>
+        <Button size="sm" variant="outlined" disabled>
+          미리보기
+        </Button>
+      </StDescriptionWrapper>
+      <ImageDropZone
+        method={method}
+        label="pcImageFileName"
+        width="580px"
+        height="170px"
+        required
+      />
+
+      <StDescriptionWrapper style={{ marginTop: '2rem' }}>
+        <StDescription>
+          <StDescriptionTitle>[PC]</StDescriptionTitle> 이미지는 1824*328 px
+          크기로 올려주세요.
+          <p>(형식: PNG, 용량: 5MB 이내)</p>
+        </StDescription>
+        <Button size="sm" variant="outlined" disabled>
+          미리보기
+        </Button>
+      </StDescriptionWrapper>
+      <ImageDropZone
+        method={method}
+        label="mobileImageFileName"
+        width="580px"
+        height="170px"
+        required
+      />
+    </StContentWrapper>
+  );
+};
+
+export default BannerImageRegister;
+
+const StDescriptionWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
