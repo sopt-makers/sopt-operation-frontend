069c59668cf6d5c987ff33ce8e5a048db450b25f
namdaeun <nde40345@gmail.com>
2025-03-29 17:37:57 +0900

    feat: 배너 관리 아이템 구현

---
+++ src/components/bannerAdmin/ListItem/ListItem.tsx
@@ -0,0 +1,126 @@
+import { IcEdit, IcTrash } from '@/assets/icons';
+import BannerTag from '@/components/bannerAdmin/BannerTag/BannerTag';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
+import { fontsObject } from '@sopt-makers/fonts';
+
+const ListItem = () => {
+  const ITEM_LIST = [
+    {
+      status: '진행 예정',
+      bannerLocation: '플그 커뮤니티',
+      contentType: '프로덕트 홍보',
+      requester: '마케팅 오거나이저',
+      startedAt: '2025.03.28',
+      endedAt: '2025.03.28',
+    },
+  ];
+
+  return (
+    <StItemWrapper>
+      {ITEM_LIST.map((item) => (
+        <StItem>
+          <StStatus status={item.status}>{item.status}</StStatus>
+          <StBannerTagWrapper>
+            <BannerTag color="rgba(88, 207, 5, 0.50)">
+              <p>{item.bannerLocation}</p>
+            </BannerTag>
+          </StBannerTagWrapper>
+          <StBannerTagWrapper>
+            <BannerTag color={`${colors.gray700}`}>
+              {item.contentType}
+            </BannerTag>
+          </StBannerTagWrapper>
+          <p>{item.requester}</p>
+          <p>{item.startedAt}</p>
+          <p>{item.endedAt}</p>
+          <StButtonLayout>
+            <IcEdit />
+            <IcTrash />
+          </StButtonLayout>
+        </StItem>
+      ))}
+    </StItemWrapper>
+  );
+};
+
+export default ListItem;
+
+export const StItemWrapper = styled.li`
+  display: flex;
+
+  width: 100%;
+
+  padding: 0 3.4rem 0 0rem;
+
+  border: 1px solid ${colors.gray700};
+  border-radius: 1rem;
+
+  align-items: center;
+
+  &:hover {
+    background-color: ${colors.gray800};
+  }
+`;
+
+export const StStatus = styled.h4<{ status: string }>`
+  ${fontsObject.TITLE_6_16_SB}
+
+  ${({ status }) => {
+    if (status === '진행 예정') {
+      return css`
+        color: ${colors.secondary};
+      `;
+    }
+    if (status === '진행 중') {
+      return css`
+        color: ${colors.success};
+      `;
+    }
+    if (status === '진행 종료') {
+      return css`
+        color: ${colors.error};
+      `;
+    }
+  }}
+`;
+
+export const StItem = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1.1fr 1fr 1fr 1fr 0.2fr;
+
+  width: 100%;
+
+  padding: 3.3rem 0;
+
+  text-align: center;
+
+  & > p {
+    color: ${colors.gray100};
+
+    ${fontsObject.BODY_3_14_M}
+  }
+
+  & > p:nth-child(6) {
+    text-align: left;
+    margin-left: 1.5rem;
+  }
+`;
+
+export const StButtonLayout = styled.div`
+  display: flex;
+
+  padding: 0.4rem;
+
+  align-items: center;
+  gap: 1.2rem;
+
+  cursor: pointer;
+  box-sizing: content-box;
+`;
+
+const StBannerTagWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
