c45f5516e9d4b12cdbe7976aa497b4c3ee5e919b
namdaeun <nde40345@gmail.com>
2025-03-31 21:11:40 +0900

    feat: 제목 추가

--- src/pages/bannerAdmin/index.tsx
+++ src/pages/bannerAdmin/index.tsx
@@ -1,20 +1,38 @@
 import Header from '@/components/bannerAdmin/Header/Header';
 import ListItem from '@/components/bannerAdmin/ListItem/ListItem';
+import Tabs from '@/components/bannerAdmin/Tabs/Tabs';
 import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
+import { fontsObject } from '@sopt-makers/fonts';
 const index = () => {
   return (
-    <div>
+    <StWrapper>
+      <StTitle>광고 배너 관리</StTitle>
+      <Tabs />
       <StListWrapper>
         <Header />
         <StItemWrapper>
           <ListItem />
         </StItemWrapper>
       </StListWrapper>
-    </div>
+    </StWrapper>
   );
 };
 
 export default index;
+
+const StWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 4.9rem;
+`;
+
+const StTitle = styled.h1`
+  color: ${colors.white};
+
+  ${fontsObject.TITLE_1_32_SB}
+`;
 
 const StListWrapper = styled.ul`
   display: flex;
