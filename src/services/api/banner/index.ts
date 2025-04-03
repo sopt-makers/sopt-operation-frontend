a0c08c9e8bb37fa8a555751b297a69ea107ecac4
namdaeun <nde40345@gmail.com>
2025-04-03 22:22:13 +0900

    feat: sort, tab에 따른 쿼리 스트링 부여

--- src/services/api/banner/index.ts
+++ src/services/api/banner/index.ts
@@ -1,9 +1,26 @@
 import { client } from '@/services/api/client';
 import { AxiosResponse } from 'axios';
 
-export const fetchBannerList = async () => {
+export const fetchBannerList = async (
+  status: string = '',
+  sort: string = 'status',
+) => {
+  if (!status && sort === 'status') {
+    const { data }: AxiosResponse<{ success: boolean; data: Banner[] }> =
+      await client.get('/banners');
+    return data.data;
+  }
+
+  let queryString = '';
+
+  if (status) {
+    queryString = `?status=${status}`;
+  }
+
+  queryString += queryString ? `&sort=${sort}` : `?sort=${sort}`;
+
   const { data }: AxiosResponse<{ success: boolean; data: Banner[] }> =
-    await client.get('/banners');
+    await client.get(`/banners${queryString}`);
 
   return data.data;
 };
--- src/services/api/banner/query.ts
+++ src/services/api/banner/query.ts
@@ -1,6 +1,11 @@
 import { fetchBannerList } from '@/services/api/banner';
 import { useQuery } from 'react-query';
 
-export const useFetchBannerList = () => {
-  return useQuery('bannerList', fetchBannerList);
+export const useFetchBannerList = (
+  status: string = '',
+  sort: string = 'status',
+) => {
+  return useQuery(['bannerList', status, sort], () =>
+    fetchBannerList(status, sort),
+  );
 };
