42157fdd9ff7457418e6c3921d0ebc25f5b56ec5
namdaeun <nde40345@gmail.com>
2026-02-12 12:17:12 +0900

    feat: 최신 소식 presigned url사용

--- src/components/org/OrgAdmin/HomeSection/api.ts
+++ src/components/org/OrgAdmin/HomeSection/api.ts
@@ -1,10 +1,15 @@
 import axios from 'axios';
-
 import config from '@/configs/config';
 import { getToken } from '@/utils/auth';
 import { ACTIVITY_GENERATION } from '@/utils/generation';
-
 import { fetcher } from '../api';
+
+interface PresignedUrlResponse {
+  presignedUrl: string;
+  fileUrl: string;
+  expiresIn: number;
+  fileKey: string;
+}
 
 export const getAdminInfo = async () => {
   const { data } = await fetcher.GET('/admin', {
@@ -18,6 +23,60 @@ export const getAdminInfo = async () => {
   return data;
 };
 
+export const getPresignedUrl = async (
+  file: File,
+): Promise<PresignedUrlResponse> => {
+  const extension = file.name.split('.').pop();
+  const fileName = `news-${Date.now()}.${extension}`;
+
+  const { data } = await axios.post<PresignedUrlResponse>(
+    `${config.ORG_API_URL}/v2/s3/presigned-url`,
+    {
+      fileName,
+      contentType: file.type,
+      directory: 'news/',
+    },
+    {
+      headers: {
+        Authorization: getToken('ACCESS'),
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  return data;
+};
+
+export const uploadToS3 = async (presignedUrl: string, file: File) => {
+  const res = await axios.put(presignedUrl, file, {
+    headers: {
+      'Content-Type': file.type,
+    },
+  });
+
+  return res;
+};
+
+export const postNewsV2 = async (data: {
+  imageUrl: string;
+  title: string;
+  link: string;
+}) => {
+  const res = await axios.post(
+    `${config.ORG_API_URL}/v2/admin/news/v2`,
+    data,
+    {
+      headers: {
+        Authorization: getToken('ACCESS'),
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  return res;
+};
+
+/** 최신소식 추가 (기존 멀티파트 방식 - deprecated) */
 export const postNews = async (formData: FormData) => {
   const res = await axios.post(
     `${config.ORG_API_URL}/v2/admin/news`,
