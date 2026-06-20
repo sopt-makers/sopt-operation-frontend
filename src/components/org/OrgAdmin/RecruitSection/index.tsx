b8e696861f295b38db95b270ac7439c6c687f8c2
namdaeun <nde40345@gmail.com>
2026-06-21 00:35:08 +0900

    feat: RecruitSection 하위 컴포넌트에 모집 데이터 전달

--- src/components/org/OrgAdmin/RecruitSection/index.tsx
+++ src/components/org/OrgAdmin/RecruitSection/index.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { useAdminInfoQuery } from '@/components/org/OrgAdmin/HomeSection/queries';
+import CurriculumSection from '@/components/org/OrgAdmin/RecruitSection/_components/Curriculum';
+import FaqSection from '@/components/org/OrgAdmin/RecruitSection/_components/Faq';
+import HeaderSection from '@/components/org/OrgAdmin/RecruitSection/_components/Header';
+import PartIntroSection from '@/components/org/OrgAdmin/RecruitSection/_components/PartIntroduction';
 import { PART_KO } from '@/utils/org';
 
 import { StContainer } from '../style';
-import Fna from './Fna';
-import Header from './Header';
-import PartCurriculum from './PartCurriculum';
 
 interface RecruitSectionProps {
+  introPart: PART_KO;
+  onChangeIntroPart: (part: PART_KO) => void;
   curriculumPart: PART_KO;
   onChangeCurriculumPart: (part: PART_KO) => void;
   fnaPart: PART_KO;
@@ -13,19 +20,53 @@ interface RecruitSectionProps {
 }
 
 const RecruitSection = ({
+  introPart,
+  onChangeIntroPart,
   curriculumPart,
   onChangeCurriculumPart,
   fnaPart,
   onChangeFnaPart,
 }: RecruitSectionProps) => {
+  const { data } = useAdminInfoQuery();
+  const { setValue } = useFormContext();
+
+  useEffect(() => {
+    if (!data) return;
+
+    data.partIntroduction?.forEach(({ part, description }) => {
+      setValue(`partIntroduction${part}`, description ?? '');
+    });
+
+    data.partCurriculum?.forEach(({ part, curriculums }) => {
+      curriculums?.forEach((curriculum, idx) => {
+        setValue(`partCurriculum.${part}.${idx}`, curriculum ?? '');
+      });
+    });
+
+    data.recruitPartCurriculum?.forEach(({ part, introduction }) => {
+      setValue(
+        `recruitPartCurriculum.${part}.content`,
+        introduction?.content ?? '',
+      );
+      setValue(
+        `recruitPartCurriculum.${part}.preference`,
+        introduction?.preference ?? '',
+      );
+    });
+  }, [data, setValue]);
+
   return (
     <StContainer>
-      <Header />
-      <PartCurriculum
-        curriculumPart={curriculumPart}
-        onChangeCurriculumPart={onChangeCurriculumPart}
+      <HeaderSection />
+      <PartIntroSection
+        selectedPart={introPart}
+        onChangePart={onChangeIntroPart}
       />
-      <Fna fnaPart={fnaPart} onChangeFnaPart={onChangeFnaPart} />
+      <CurriculumSection
+        selectedPart={curriculumPart}
+        onChangeSelectedPart={onChangeCurriculumPart}
+      />
+      <FaqSection fnaPart={fnaPart} onChangeFnaPart={onChangeFnaPart} />
     </StContainer>
   );
 };
