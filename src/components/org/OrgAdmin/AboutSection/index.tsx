14ae08f1ac070ad40139e2dda145dd12113f1d4f
constantly-dev <rntqhdl10@naver.com>
2026-06-28 19:04:54 +0900

    feat: 소개 탭 편집/배포 플로우 연결
    EditActionBar/ActionModal 연동으로 편집·배포 스텝 관리,
    useDeployAboutMutation으로 배포 요청 처리,
    syncAboutFormFromAdminData로 서버 데이터 폼 초기화 및 취소 시 복원,
    ToastProvider 래핑 추가.

--- src/components/org/OrgAdmin/AboutSection/index.tsx
+++ src/components/org/OrgAdmin/AboutSection/index.tsx
@@ -1,32 +1,119 @@
+import { ToastProvider } from '@sopt-makers/ui';
+import { useEffect, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { ActionModal } from '@/components/org/OrgAdmin/common/ActionModal';
+import {
+  EDIT_STEP,
+  type EditStep,
+} from '@/components/org/OrgAdmin/common/constants/editStep';
+import { EditActionBar } from '@/components/org/OrgAdmin/common/EditActionBar';
+import { useAdminInfoQuery } from '@/components/org/OrgAdmin/HomeSection/queries';
+import { validationAboutInputs } from '@/components/org/OrgAdmin/utils';
 import { EXEC_TYPE } from '@/utils/org';
 
-import Curriculum from '../RecruitSection/_components/Curriculum';
+import { StContainer } from '../style';
 import CoreValue from './CoreValue';
 import Executives from './Executives';
+import { syncAboutFormFromAdminData } from './formMapper';
 import HeaderBanner from './HeaderBanner';
+import { useDeployAboutMutation } from './queries';
 import Schedule from './Schedule';
-import { StContainer } from './style';
 
 interface AboutSectionProps {
   selectedExec: EXEC_TYPE;
   onChangeSelectedExec: (member: EXEC_TYPE) => void;
 }
 
-const AboutSection = ({
+const AboutSectionContent = ({
   selectedExec,
   onChangeSelectedExec,
 }: AboutSectionProps) => {
+  const [editStep, setEditStep] = useState<EditStep>(EDIT_STEP.VIEW);
+
+  const { data } = useAdminInfoQuery();
+  const { mutate: deployAbout, isLoading: isDeploying } =
+    useDeployAboutMutation();
+
+  const {
+    getValues,
+    setValue,
+    setError,
+    clearErrors,
+    formState: { isDirty },
+  } = useFormContext();
+
+  const isEditMode = editStep !== EDIT_STEP.VIEW;
+  const isDeployModalOpen = editStep === EDIT_STEP.DEPLOY;
+  const hasUnsavedChanges = isEditMode && isDirty;
+
+  useEffect(() => {
+    syncAboutFormFromAdminData(data, setValue);
+  }, [data, setValue]);
+
+  const exitEditMode = () => {
+    syncAboutFormFromAdminData(data, setValue);
+    clearErrors();
+    setEditStep(EDIT_STEP.VIEW);
+  };
+
+  const handleDeploy = () => {
+    deployAbout(
+      { values: getValues() },
+      {
+        onSuccess: () => setEditStep(EDIT_STEP.VIEW),
+      },
+    );
+  };
+
   return (
     <StContainer>
-      <HeaderBanner />
-      <CoreValue />
-      {/* Curriculum은 추후 제거 예정이라 잠시 비워두고, 현재는 전체 일정으로 대체한다. */}
-      <Schedule />
+      <EditActionBar
+        isEditMode={isEditMode}
+        isDeploying={isDeploying}
+        hasUnsavedChanges={hasUnsavedChanges}
+        onStartEdit={() => setEditStep(EDIT_STEP.EDITING)}
+        onCancel={exitEditMode}
+        onDeploy={() => {
+          if (
+            validationAboutInputs(
+              getValues,
+              setError,
+              data?.headerImage,
+              data?.coreValue,
+              data?.member,
+            )
+          ) {
+            setEditStep(EDIT_STEP.DEPLOY);
+          }
+        }}
+      />
+      <HeaderBanner isEditable={isEditMode} />
+      <CoreValue isEditable={isEditMode} />
+      <Schedule isEditable={isEditMode} />
       <Executives
+        isEditable={isEditMode}
         selectedExec={selectedExec}
         onChangeSelectedExec={onChangeSelectedExec}
       />
+
+      <ActionModal
+        variant="deploy"
+        isOpen={isDeployModalOpen}
+        onCancel={() => setEditStep(EDIT_STEP.EDITING)}
+        onAction={handleDeploy}
+        alertText="배포하시겠습니까?"
+        description="입력한 소개 탭 내용은 공홈에 즉시 반영돼요."
+      />
     </StContainer>
+  );
+};
+
+const AboutSection = (props: AboutSectionProps) => {
+  return (
+    <ToastProvider>
+      <AboutSectionContent {...props} />
+    </ToastProvider>
   );
 };
 
