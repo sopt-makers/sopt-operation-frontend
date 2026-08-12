74ef783ee299e4e1a77b1661c6284c70a962c05b
namdaeun <nde40345@gmail.com>
2026-08-12 23:44:59 +0900

    feat: 39기 드롭다운 추가

--- src/utils/generation.ts
+++ src/utils/generation.ts
@@ -1,6 +1,7 @@
-export const ACTIVITY_GENERATION: string = '38';
+export const ACTIVITY_GENERATION: string = '39';
 
 export const GENERATION_LIST: string[] = [
+  '39',
   '38',
   '37',
   '36',
--- src/utils/nav.ts
+++ src/utils/nav.ts
@@ -10,7 +10,9 @@ const MAKERS_APPLICATION_URL = 'https://recruiting-admin.sopt.org/';
 const SOPT_APPLICATION_URL = 'https://recruit-admin.sopt.org/';
 
 export const GENERATION_INFO = [
-  { generation: '38', slogan: `LET'S` },
+  { generation: '39', slogan: 'PLAY' },
+  // eslint-disable-next-line prettier/prettier
+  { generation: '38', slogan: 'LET\'S' },
   { generation: '37', slogan: 'DIVE' },
   { generation: '36', slogan: 'AT' },
   {
